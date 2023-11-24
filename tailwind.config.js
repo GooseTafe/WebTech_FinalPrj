59be029972f8c9766ef4c45576f27d9e764e7895
GooseTafe <20085553@tafe.wa.edu.au>
2023-11-24 15:49:28 -0800

    feat: tailwindcss config creation

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './about/*.html',
+    './contact/*.html',
+    './resume/*.html',
+    './projects/*.html',
+    './example_projects/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
